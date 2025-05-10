0c5da8cc5c788590c6f1c9684aa5e00c51559aa1
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2025-05-10 08:55:52 -0500

    feat: ADVRTE-1 MUI Material CSS Setup

--- client/main.tsx
+++ client/main.tsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Meteor } from 'meteor/meteor';
-import { App } from '/imports/ui/App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import { Meteor } from "meteor/meteor";
+
+import { App } from "/imports/ui/App";
 
 Meteor.startup(() => {
-  const container = document.getElementById('react-target');
+  const container = document.getElementById("react-target");
   const root = createRoot(container!);
-  root.render(<App />);
+
+  const DARK_THEME = createTheme({
+    palette: { mode: "dark" },
+  });
+
+  root.render(
+    <ThemeProvider theme={DARK_THEME}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  );
 });
