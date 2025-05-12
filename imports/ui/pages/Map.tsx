c439ac64a2f25e59c7be3ab890de97332960d3f8
Kevin Nguyen <kevinvannguyen1997@gmail.com>
2025-05-11 21:25:08 -0500

    feat: ADVRTE-4 Map API and UI (#4)
    * feat: ADVRTE-4 Map API and UI
    
    * google maps api key private

--- imports/ui/pages/Map.tsx
+++ imports/ui/pages/Map.tsx
@@ -1,6 +1,27 @@
-import React from "react";
-import { Box } from "@mui/material";
+import React, { useState, CSSProperties } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const MAP_CONTAINER_STYLE: CSSProperties = {
+  width: "100%",
+  height: "calc(100vh - 84px)",
+};
 
 export const Map = () => {
-  return <Box>Map</Box>;
+  const [map, setMap] = useState<google.maps.Map | null>(null);
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "YOUR_API_KEY",
+  });
+
+  const onLoad = (map: google.maps.Map) => {
+    const bounds = new google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  };
+
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
+  return <GoogleMap mapContainerStyle={MAP_CONTAINER_STYLE} onLoad={onLoad} />;
 };
