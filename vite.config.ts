ca69813c0138263f7f10db2856e33e204ae1b316
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2025-05-24 21:21:27 -0500

    proxy setup, added env examples

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,31 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:8080",
+          changeOrigin: false,
+          secure: false,
+          ws: true,
+          configure: (proxy, _options) => {
+            proxy.on("error", (err, _req, _res) => {
+              console.log("proxy error", err);
+            });
+            proxy.on("proxyReq", (_proxyReq, req, _res) => {
+              console.log("Sending Request to Target:", req.method, req.url);
+            });
+            proxy.on("proxyRes", (proxyRes, req, _res) => {
+              console.log(
+                "Received Response from the Target:",
+                proxyRes.statusCode,
+                req.url
+              );
+            });
+          },
+        },
+      },
+    },
     define: {
       __APP_ENV__: JSON.stringify(env.APP_ENV),
     },
