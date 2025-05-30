3274c0173287e3d3edc3a1e287b63b799d6be206
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2025-05-30 14:49:02 -0500

    unique indexes

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,25 +1,32 @@
-import * as mongoDB from "mongodb";
+import { Collection, Db, MongoClient } from "mongodb";
 import env from "@constants/env";
+import type Route from "@models/route";
+import type User from "@models/user";
 
 type AdventureRouteDBCollections = {
-  users?: mongoDB.Collection;
-  routes?: mongoDB.Collection;
-  comments?: mongoDB.Collection;
+  users?: Collection<User>;
+  routes?: Collection<Route>;
+  comments?: Collection;
 };
 export const collections: AdventureRouteDBCollections = {};
 
 export const connectToDatabase = async () => {
-  const client: mongoDB.MongoClient = new mongoDB.MongoClient(
-    env.VITE_DB_CONNECTION_STRING!
-  );
+  const client: MongoClient = new MongoClient(env.VITE_DB_CONNECTION_STRING!);
 
   await client.connect();
 
-  const db: mongoDB.Db = client.db("adventure-routes");
+  const db: Db = client.db("adventure-routes");
 
-  const usersCollection: mongoDB.Collection = db.collection("users");
-  const routesCollection: mongoDB.Collection = db.collection("routes");
-  const commentsCollection: mongoDB.Collection = db.collection("comments");
+  const usersCollection: Collection<User> = db.collection<User>("users");
+  const routesCollection: Collection<Route> = db.collection<Route>("routes");
+  const commentsCollection: Collection = db.collection("comments");
+
+  usersCollection.createIndex(
+    { id: 1, username: 1, email: 1 },
+    { unique: true }
+  );
+  routesCollection.createIndex({ id: 1 });
+  commentsCollection.createIndex({ id: 1 });
 
   collections.users = usersCollection;
   collections.routes = routesCollection;
