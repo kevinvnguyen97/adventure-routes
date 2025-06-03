34f5be4aece4166513aa08239f7183b6ee2887ec
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2025-06-02 22:07:26 -0500

    initial dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,14 @@
+import { Button, Input, VStack } from "@chakra-ui/react";
+
 const Dashboard = () => {
-  return <></>;
+  return (
+    <VStack alignItems="center">
+      <VStack width={{ smDown: "100%", sm: 400 }}>
+        <Input placeholder="Search for route" />
+        <Button>Create a Route</Button>
+      </VStack>
+    </VStack>
+  );
 };
 
 export default Dashboard;
