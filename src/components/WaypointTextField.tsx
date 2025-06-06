5011a2f56696d0115836e091c71a23bfb77346ac
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2025-06-05 21:28:33 -0500

    draggable waypoints

---
+++ src/components/WaypointTextField.tsx
@@ -0,0 +1,122 @@
+import {
+  Field,
+  InputGroup,
+  Box,
+  IconButton,
+  Input,
+  ButtonGroup,
+} from "@chakra-ui/react";
+import { useSortable } from "@dnd-kit/sortable";
+import {
+  LuMapPin,
+  LuMinus,
+  LuPlus,
+  LuArrowDownUp,
+  LuArrowDown,
+  LuArrowUp,
+} from "react-icons/lu";
+import { CSS } from "@dnd-kit/utilities";
+
+type WaypointTextFieldProps = {
+  waypoint: WaypointInput;
+  stopNumber: number;
+  isOrigin?: boolean;
+  isDestination?: boolean;
+  hasMoreWaypoints?: boolean;
+  addWaypoint: () => void;
+  removeWaypoint: () => void;
+  onWaypointChange: (newWaypointValue: string) => void;
+};
+const WaypointTextField = (props: WaypointTextFieldProps) => {
+  const {
+    waypoint,
+    stopNumber,
+    isOrigin,
+    isDestination,
+    hasMoreWaypoints,
+    addWaypoint,
+    removeWaypoint,
+    onWaypointChange,
+  } = props;
+
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: waypoint.id });
+
+  return (
+    <Field.Root
+      orientation="horizontal"
+      ref={setNodeRef}
+      style={{ transform: CSS.Transform.toString(transform) }}
+      // transform={CSS.Transform.toString(transform)}
+      transition={transition}
+      opacity={isDragging ? 0.5 : 1}
+      touchAction="none"
+    >
+      <Field.Label>
+        {isOrigin
+          ? "Origin"
+          : isDestination
+          ? "Destination"
+          : `Stop #${stopNumber}`}
+      </Field.Label>
+      <InputGroup
+        startElement={<LuMapPin />}
+        endAddonProps={{ variant: "subtle", paddingLeft: 0, paddingRight: 0 }}
+        endAddon={
+          <ButtonGroup gap={0}>
+            {hasMoreWaypoints && (
+              <IconButton
+                onClick={removeWaypoint}
+                variant="ghost"
+                color="red"
+                colorPalette="red"
+                size="xs"
+              >
+                <LuMinus />
+              </IconButton>
+            )}
+            <IconButton
+              onClick={addWaypoint}
+              variant="ghost"
+              color={{ _light: "green", _dark: "lightgreen" }}
+              colorPalette="green"
+              size="xs"
+            >
+              <LuPlus />
+            </IconButton>
+            <IconButton
+              {...attributes}
+              {...listeners}
+              variant="ghost"
+              color={{ _light: "black", _dark: "white" }}
+              size="xs"
+            >
+              {isOrigin ? (
+                <LuArrowDown />
+              ) : isDestination ? (
+                <LuArrowUp />
+              ) : (
+                <LuArrowDownUp />
+              )}
+            </IconButton>
+          </ButtonGroup>
+        }
+      >
+        <Input
+          value={waypoint.text}
+          onChange={(e) => onWaypointChange(e.target.value)}
+          placeholder="Enter address or place of interest"
+          variant="subtle"
+        />
+      </InputGroup>
+    </Field.Root>
+  );
+};
+
+export default WaypointTextField;
