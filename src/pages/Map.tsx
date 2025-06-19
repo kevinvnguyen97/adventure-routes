9999b869bcb53acc23c98cb106a3d316262742ef
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2025-06-18 21:21:00 -0500

    show toaster for map render

--- src/pages/Map.tsx
+++ src/pages/Map.tsx
@@ -12,6 +12,7 @@ import { Box, IconButton } from "@chakra-ui/react";
 import Loading from "@components/Loading";
 import { RouteColors } from "@constants/google";
 import TripDetails from "@components/TripDetails";
+import { toaster } from "@utils/toaster";
 
 const Map = () => {
   const { tripId = "" } = useParams();
@@ -24,9 +25,11 @@ const Map = () => {
   >();
   const [isTripRendered, setIsTripRendered] = useState(false);
   const [isInfoVisible, setIsInfoVisible] = useState(false);
+  const [areRouteAlternativesAllowed, setAreRouteAlternativesAllowed] =
+    useState(true);
 
   const { trip, isLoading } = useTrip(tripId);
-  const { waypoints = [] } = trip || {};
+  const { name = "", waypoints = [] } = trip || {};
   const origin = waypoints[0];
   const stops: google.maps.DirectionsWaypoint[] =
     waypoints.length <= 2
@@ -55,11 +58,98 @@ const Map = () => {
   ) => {
     if (directions) {
       return;
-    } else if (status === google.maps.DirectionsStatus.OK && !directions) {
-      console.log("Directions response:", response);
-      setDirections(response as google.maps.DirectionsResult | undefined);
-    } else {
-      console.error("Error fetching directions:", status);
+    }
+    switch (status) {
+      // Directions successfully fetched
+      case google.maps.DirectionsStatus.OK:
+        setDirections(response as google.maps.DirectionsResult);
+        toaster.create({
+          title: "Success",
+          description: `Routes found successfully for trip "${name}"`,
+          type: "success",
+          closable: true,
+        });
+        break;
+      case google.maps.DirectionsStatus.INVALID_REQUEST:
+        toaster.create({
+          title: "Invalid Request",
+          description: "Route cannot be rendered",
+          type: "error",
+          closable: true,
+        });
+        break;
+      // At least one waypoint cannot be found
+      case google.maps.DirectionsStatus.NOT_FOUND:
+        toaster.create({
+          title: "Not Found",
+          description: "At least one waypoint is not found",
+          type: "error",
+          closable: true,
+        });
+        break;
+      // No routes can be found between origin and destination
+      case google.maps.DirectionsStatus.ZERO_RESULTS:
+        toaster.create({
+          title: "Zero Results",
+          description: "No routes found between origin and destination",
+          type: "error",
+          closable: true,
+        });
+        break;
+      // Too many waypoints
+      case google.maps.DirectionsStatus.MAX_WAYPOINTS_EXCEEDED:
+        toaster.create({
+          title: "Too Many Waypoints",
+          description: "Too many waypoints added to trip",
+          type: "error",
+          closable: true,
+        });
+        break;
+      // Query limit reached
+      case google.maps.DirectionsStatus.OVER_QUERY_LIMIT:
+        toaster.create({
+          title: "Too Many Requests",
+          description: "Please wait a few minutes to reset query limit",
+          type: "error",
+          closable: true,
+        });
+        break;
+      // Webpage denied to use directions service
+      case google.maps.DirectionsStatus.REQUEST_DENIED:
+        toaster.create({
+          title: "Request Denied",
+          description: "Directions service is not allowed",
+          type: "error",
+          closable: true,
+        });
+        break;
+      // @ts-expect-error This error exists but
+      // is not part of google.maps.DirectionsStatus enum
+      case "MAX_ROUTE_LENGTH_EXCEEDED": {
+        const numOfRoutes = response?.routes?.length ?? 0;
+        if (numOfRoutes <= 1) {
+          toaster.create({
+            title: "Zero Results",
+            description: "No routes found between origin and destination",
+            type: "error",
+            closable: true,
+          });
+          break;
+        }
+        setAreRouteAlternativesAllowed(false);
+        setDirections(undefined);
+        break;
+      }
+      // Catch other errors
+      default:
+        console.error("Error fetching directions:", status);
+        toaster.create({
+          title: "Unknown Error",
+          description: "Server error occured",
+          type: "error",
+          closable: true,
+        });
+        break;
     }
   };
 
@@ -119,7 +209,7 @@ const Map = () => {
             waypoints: stops,
             destination,
             travelMode: google.maps.TravelMode.DRIVING,
-            provideRouteAlternatives: true,
+            provideRouteAlternatives: areRouteAlternativesAllowed,
           }}
           callback={directionsServiceCallback}
         />
