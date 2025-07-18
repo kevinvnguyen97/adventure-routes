8bb0e24a8da608d4f7341bd8417874e6a588f386
kevinvnguyen97 <kevinvannguyen1997@gmail.com>
2025-07-18 09:24:30 -0500

    initial settings page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,31 +1,79 @@
-import { VStack, Text } from "@chakra-ui/react";
-import { ColorModeButton } from "@components/ui/color-mode";
+import { VStack, Text, HStack, Table, IconButton } from "@chakra-ui/react";
 import UserAvatar from "@components/UserAvatar";
 import { useAuth } from "@utils/auth";
 import { useLayoutEffect } from "react";
+import { LuPencil } from "react-icons/lu";
 
 const Settings = () => {
   const { user } = useAuth();
+
+  const { firstName, lastName, username, email } = user || {};
+
+  const name = [firstName, lastName].filter(Boolean).join(" ");
 
   useLayoutEffect(() => {
     window.document.title = "Settings - Adventure Routes";
   }, []);
 
   return (
-    <VStack data-state="open" _open={{ animation: "fade-in 1s ease-out" }}>
+    <VStack
+      data-state="open"
+      _open={{ animation: "fade-in 1s ease-out" }}
+      marginTop={5}
+    >
       <UserAvatar
         fallbackProps={{ name: user?.username }}
         variant="subtle"
         _hover={{ cursor: "pointer" }}
         size="2xl"
       />
-      <Text color="white">
-        {[user?.firstName, user?.lastName].filter(Boolean).join(" ")}
-      </Text>
-      <ColorModeButton
-        color="white"
-        _hover={{ bgColor: { _light: "orange.600" } }}
-      />
+      <Table.Root width={500} variant="outline" size="lg">
+        <Table.Row>
+          <Table.Cell fontWeight="bold">Name</Table.Cell>
+          <Table.Cell>{name}</Table.Cell>
+          <Table.Cell>
+            <IconButton variant="subtle">
+              <LuPencil />
+            </IconButton>
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell fontWeight="bold">Username</Table.Cell>
+          <Table.Cell>{username}</Table.Cell>
+          <Table.Cell>
+            <IconButton variant="subtle">
+              <LuPencil />
+            </IconButton>
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell fontWeight="bold">Email</Table.Cell>
+          <Table.Cell>{email}</Table.Cell>
+          <Table.Cell>
+            <IconButton variant="subtle">
+              <LuPencil />
+            </IconButton>
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell fontWeight="bold">Phone Number</Table.Cell>
+          <Table.Cell>(708)000-0000</Table.Cell>
+          <Table.Cell>
+            <IconButton variant="subtle">
+              <LuPencil />
+            </IconButton>
+          </Table.Cell>
+        </Table.Row>
+        <Table.Row>
+          <Table.Cell fontWeight="bold">Password</Table.Cell>
+          <Table.Cell>********</Table.Cell>
+          <Table.Cell>
+            <IconButton variant="subtle">
+              <LuPencil />
+            </IconButton>
+          </Table.Cell>
+        </Table.Row>
+      </Table.Root>
     </VStack>
   );
 };
